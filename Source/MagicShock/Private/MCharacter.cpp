5ad3e03923f88f6063df1caa8b659675f047bf11
hockit <marcelmd@wp.pl>
2023-09-05 10:49:22 +0200

    Initial commit

---
+++ Source/MagicShock/Private/MCharacter.cpp
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+AMCharacter::AMCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
+	CameraComp->bUsePawnControlRotation = true;
+	CameraComp->SetupAttachment(RootComponent);
+
+	bIsCrouch = false;
+}
+
+// Called when the game starts or when spawned
+void AMCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AMCharacter::Jump);
+	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &AMCharacter::Crouch);
+}
+
+void AMCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value);
+}
+
+void AMCharacter::MoveRight(float Value)
+{
+	AddMovementInput(GetActorRightVector(), Value);
+}
+
+void AMCharacter::Crouch()
+{
+	if (!GetCharacterMovement()->IsFalling())
+	{
+		if (!bIsCrouch)
+		{
+			GetCharacterMovement()->MaxWalkSpeed = 300.f;
+			GetCapsuleComponent()->SetCapsuleHalfHeight(44.f);
+			bIsCrouch = true;			
+		}
+		else
+		{
+			GetCharacterMovement()->MaxWalkSpeed = 600.f;
+			GetCapsuleComponent()->SetCapsuleHalfHeight(88.f);
+			bIsCrouch = false;
+		}
+	}
+	
+}
